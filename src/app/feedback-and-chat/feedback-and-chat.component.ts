a820fd5448517bb1432077c8dbafff02599c8107
Javi Carrion <code-vista@outlook.com>
2022-11-16 23:42:35 -0500

    feat: :sparkles: add event handling to form

--- src/app/feedback-and-chat/feedback-and-chat.component.ts
+++ src/app/feedback-and-chat/feedback-and-chat.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-and-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './feedback-and-chat.component.html',
   styleUrls: ['./feedback-and-chat.component.scss']
 })
 export class FeedbackAndChatComponent {
+  showMsg: boolean = false;
+  private formBuilder = inject(FormBuilder);
 
+  contactForm = this.formBuilder.group({
+    fullname: '',
+    email: '',
+    comments: ''
+  });
+
+  onSubmit(): void {
+    console.log('Your feedback has been submitted', this.contactForm.value);
+    this.showMsg = true;
+    this.contactForm.reset();
+  }
 }
