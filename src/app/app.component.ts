513776c9e13931cf12b9ce5f9c5887d5fcffc9f2
Javi Carrion <code-vista@outlook.com>
2022-11-16 21:11:07 -0500

    feat: :sparkles: make app.component into a standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true
 })
 export class AppComponent {
-  title = 'ng-standalone-components';
+  title = 'photo-gallery';
 }
